da12d1253aa38f7c0ce8b3f0510f184d9127615a
ganchau <gancchau@gmail.com>
2015-05-14 15:43:19 -0400

    completed lab assignment

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -16,6 +16,27 @@
 }
 
 //define methods here
+- (NSString *)badgeMaker:(NSString *)attendee
+{
+    return [NSString stringWithFormat:@"Hello, my name is %@.", attendee];
+}
 
+- (NSArray *)batchBadgeCreator:(NSArray *)attendees
+{
+    NSMutableArray *badges = [[NSMutableArray alloc] init];
+    for (int i=0; i<[attendees count]; i++) {
+        badges[i] = [self badgeMaker:attendees[i]];
+    }
+    return [badges copy];
+}
+
+- (NSArray *)assignRooms:(NSArray *)attendees
+{
+    NSMutableArray *roomAssignments = [[NSMutableArray alloc] init];
+    for (int i=0; i<[attendees count]; i++) {
+        roomAssignments[i] = [NSString stringWithFormat:@"Hello, %@! You'll be assigned to room %i!", attendees[i], i+1];
+    }
+    return [roomAssignments copy];
+}
 
 @end
